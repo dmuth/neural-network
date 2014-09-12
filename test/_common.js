a94a8a1eefe4bda6200f05595cfbf032d182d9b1
Douglas Muth <dmuth@dmuth.org>
2014-09-11 22:14:20 -0400

    Updates

--- test/_common.js
+++ test/_common.js
@@ -8,10 +8,13 @@ var winston = require("winston");
 //
 // Set up our console logger.
 //
+var level = "info";
+//var level = "debug";
+
 var options = {
 	colorize: true,
 	timestamp: true,
-	level: "info"
+	level: level,
 	};
 winston.remove(winston.transports.Console);
 winston.add(winston.transports.Console, options)
