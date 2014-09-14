73ca584d788bee7fd41b859027f65d48bfaeeedf
Douglas Muth <dmuth@dmuth.org>
2014-09-14 01:22:43 -0400

    Formatting tweak

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ TODO:
 		- add(r, g, b)
 		- length() - return number of pieces of training data
 		- train()
-			- create new brain object
+			- create new brain object and train
 		- guess()
 - conditionally display train button, otherwise require 5 pieces of training data
 - /guess endpoint that shows like 10 small boxes, and guesses
