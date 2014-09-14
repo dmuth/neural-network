caa570c7e6d9d7f7383216e4615fd06d22574538
Douglas Muth <dmuth@dmuth.org>
2014-09-13 20:29:15 -0400

    Added some tests for brain

--- test/nn.js
+++ test/nn.js
@@ -5,6 +5,7 @@
 
 var _common = require("./_common");
 var should = require("should");
+var brain = require("brain");
 
 
 suite("Neural Network", function() {
@@ -18,35 +19,38 @@ suite("Neural Network", function() {
 	});
 
 
-	test("train", function(done) {
+	test("brain XOR", function(done) {
 
-		var nn_module = require("../lib/nn");
-
-		var num_nodes_input = 2;
-		//var num_nodes_hidden = 2;
-		var num_nodes_hidden = 3;
-		var num_nodes_output = 1;
-		var nn = new nn_module(num_nodes_input, num_nodes_output, num_nodes_hidden);
+		var net = new brain.NeuralNetwork();
 
 		var data = [
-			//{input: [0, 0], target: [1]},
-			//{input: [0, 1], target: [0]},
-			{input: [0.01, 0.01], target: [1]},
-			{input: [0.01, 1], target: [0]},
-			//{input: [1, 0], target: [0]},
-			//{input: [1, 1], target: [1]},
+			{input: [0, 0], output: [0]},
+			{input: [0, 1], output: [1]},
+			{input: [1, 0], output: [1]},
+			{input: [1, 1], output: [0]},
+			{input: [-1, -1], output: [.5]}, // A little extra test :-)
 			];
+		var options = {
+			errorThresh: 0.001,
+			iterations: 100000,
+			//log: true, // Debugging
+			learningRate: 0.3,
+			};
+		net.train(data, options);
 
-		var num_trains = 2;
-		//var num_trains = 3;
-		//var num_trains = 5;
-		var num_trains = 10;
-		//var num_trains = 100;
-		//var num_trains = 1000;
-		var result = nn.train(data, num_trains);
-console.log("TEST RESULT", result);
+		for (var k in data) {
+			var row = data[k];
+			var output = net.run(row.input);
+			//console.log("Debugging", row.input, output);
+		}
 
-// TODO: Test with wrong number of inputs
+		var diff = 0.1;
+		net.run(data[0].input).should.be.below(data[0].output[0] + diff);
+		net.run(data[1].input).should.be.above(data[1].output[0] - diff);
+		net.run(data[2].input).should.be.above(data[2].output[0] - diff);
+		net.run(data[3].input).should.be.below(data[3].output[0] + diff);
+		net.run(data[4].input).should.be.below(data[4].output[0] + diff);
+		net.run(data[4].input).should.be.above(data[4].output[0] - diff);
 
 		done();
 
