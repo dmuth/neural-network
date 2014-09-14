00f5f777d14eb79426a5336ddeec2b5da3e2a04a
Douglas Muth <dmuth@dmuth.org>
2014-09-14 13:00:21 -0400

    Choosing logger based on environment

--- app.js
+++ app.js
@@ -16,7 +16,12 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+if (process.env.NODE_ENV) {
+	app.use(logger('combined'));
+} else {
+	app.use(logger('dev'));
+}
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
