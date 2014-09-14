a2d201a759fa92b5f0fbe0c8cdb75ed08f2f12d4
Douglas Muth <dmuth@dmuth.org>
2014-09-13 23:31:36 -0400

    Now returning constituent color components as well.

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,14 @@ var router = express.Router();
 var colors = require("../lib/randomColor");
 
 router.get('/', function(req, res) {
+
 	var color = colors.getRandomRGB();
+
 	res.render('index', { 
 		title: "Neural Network", 
 		color: color 
 		});
+
 });
 
 module.exports = router;
